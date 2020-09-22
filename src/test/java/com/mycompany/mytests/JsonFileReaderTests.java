872b8e9a290d5714452f82363a5f77f6f87b58c3
Asad Khan <asad_khan47@hotmail.co.uk>
2020-09-22 20:14:49 +0100

    Added file checking test

--- src/test/java/com/mycompany/mytests/JsonFileReaderTests.java
+++ src/test/java/com/mycompany/mytests/JsonFileReaderTests.java
@@ -6,10 +6,17 @@ import org.json.simple.parser.ParseException;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.io.File;
 import java.io.IOException;
 
 public class JsonFileReaderTests {
     JSONObject exampleUser = CalculateDistanceTests.createExampleUser();
+
+    @Test
+    public void checkCustomersJsonExists() {
+        File customers = new File("customers.json");
+        Assert.assertTrue(String.format("File %s not found", customers), customers.exists());
+    }
 
     @Test
     public void verifyUserAtIndexZero() throws IOException, ParseException {
