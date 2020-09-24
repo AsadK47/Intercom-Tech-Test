edde2cf9d31b00f7db43563958f31fd487797393
Asad Khan <asad_khan47@hotmail.co.uk>
2020-09-24 16:46:25 +0100

    Adding check if file not present

--- src/main/java/com/mycompany/myapp/JsonFileReader.java
+++ src/main/java/com/mycompany/myapp/JsonFileReader.java
@@ -5,6 +5,8 @@ import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 
@@ -18,6 +20,13 @@ public class JsonFileReader {
 
     protected static JSONArray readJsonAndReturnArray() throws IOException, ParseException {
         JSONParser jsonParser = new JSONParser();
-        return (JSONArray) jsonParser.parse(new FileReader(customerFilePath));
+        File customerFile = new File(customerFilePath);
+        String errorMessage = String.format("The file %s does not exist", customerFile);
+
+        if (customerFile.exists() && customerFile.isFile()) {
+            return (JSONArray) jsonParser.parse(new FileReader(customerFilePath));
+        } else {
+            throw new FileNotFoundException(errorMessage);
+        }
     }
 }
